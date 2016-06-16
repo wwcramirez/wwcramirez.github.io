505ce392519d6d1da8317e8bdc66963fba684284
wwcramirez <cramirez@wovenware.com>
2016-06-16 18:36:37 -0400

    Fixed up demo form

--- src/js/modules/dashboard/dashboard.controller.js
+++ src/js/modules/dashboard/dashboard.controller.js
@@ -12,21 +12,30 @@
     function DashboardController() {
       var vm = this;
 
-      vm.showYears = false;
-      vm.showReport = true;
       //vm.title = 'Naut - Bootstrap + AngularJS (Seed Project)';
       vm.text = 'This project is an application skeleton for a typical AngularJS web app. You can use it to quickly bootstrap your angular webapp projects and dev environment for these projects.' +
                     'The seed app doesnt do much, just shows how to wire some controllers and views together.';
 
       vm.hpanel = [true, false, false, false, false];
 
+      vm.views = {
+        form: 'form',
+        years: 'years'
+      };
+
+      vm.currentView = null;
+
       vm.download = function () {
         window.open('sample.pdf');
       };
 
-      vm.toggle = function () {
-        vm.showYears = !vm.showYears;
-        vm.showReport = !vm.showReport;
+      vm.setCurrentView = function (view) {
+        vm.currentView = view;
+      };
+
+      vm.accordion = {
+        patron: true,
+        employee: true
       };
     }
 
